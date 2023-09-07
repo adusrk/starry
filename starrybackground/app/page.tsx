782923e34fb32709034c84911f7dbbf0ea20beb3
adusrk <adussrk@gmail.com>
2023-09-07 22:33:08 +0530

    Initial commit

---
+++ starrybackground/app/page.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import StarsBackground from "../components/StarBackground";
+import styles from "./App.module.css";
+
+const App: React.FC = () => {
+  return (
+    <div className={styles["square-container"]}>
+      <StarsBackground />
+      <div className={styles["shooting-star"]}>
+        {[...Array(10)].map((_, index) => (
+          <span key={index}></span>
+        ))}
+      </div>
+      <div className={styles.txt}>
+        <h2>Welcome</h2>
+        <p>To my star-y background</p>
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ starrybackground/components/StarBackground/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './styles.module.css';
+
+const StarsBackground: React.FC = () => {
+  return <div className={styles['stars-bg']} />;
+};
+
+export default StarsBackground;
